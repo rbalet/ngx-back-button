7c13b02447987cadc870b540e0383eb0b8f1df37
Raphaël Balet <raphael.balet@outlook.com>
2026-07-22 08:31:21 +0200

    build(zone.js): removal

--- projects/ngx-back-button-tester/src/app/app.component.spec.ts
+++ projects/ngx-back-button-tester/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { TestBed, waitForAsync } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 import { MatDividerModule } from '@angular/material/divider'
@@ -10,8 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
         FormsModule,
         ReactiveFormsModule,
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
         AppComponent,
       ],
     }).compileComponents()
-  }))
+  })
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent)
